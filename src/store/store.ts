905f9624fb7913125a3af938ac99448b64101569
Anastasia <ana.ko8915@gmail.com>
2023-10-26 12:27:02 +0300

    fixed loading last updated issues on reload

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,6 +3,9 @@ import issuesReducer from "../features/issuesSlice";
 
 const preloadedState = JSON.parse(localStorage.getItem("issuesState") || "{}");
 
+// Reset the currentRepoUrl on page load
+preloadedState.currentRepoUrl = "";
+
 const store = configureStore({
   reducer: {
     issues: issuesReducer,
